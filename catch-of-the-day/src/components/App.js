f5d68ce96b76233e120b2f5a8ad30477da4d24bb
Matt Gross <mattonomics@gmail.com>
2017-03-23 06:24:19 -0400

    Day 4 Lessons 14-16. Learning more about state and how components communicate and alter state between one another.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   constructor() {
     super();
 
     this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
 
     this.state = {
       fishes: {},
@@ -25,14 +29,37 @@ class App extends React.Component {
     this.setState({ fishes });
   }
 
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
+
+  addToOrder(key) {
+    // take a copy of our state
+    const order = {...this.state.order};
+    // update or add the new number of fish ordered
+    order[key] = order[key] + 1 || 1;
+    //update state
+    this.setState({ order });
+  }
+
   render() {
     return(
       <div className='catch-of-the-day'>
         <div className='menu'>
           <Header tagline='Fresh Seafood Market'/>
+          <ul className='list-of-fishes'>
+            {/* Loop over something, must have array. Object.keys() gets the array that we can then .map() over. */
+              Object
+              .keys(this.state.fishes)
+              /* key prop does not pass down to children. must use another prop, in this case, index */
+              .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)
+            }
+          </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     )
   }
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -7,8 +7,8 @@ class Inventory extends React.Component {
       <div>
         <h2>Inventory</h2>
         <AddFishForm addFish={this.addFish} />
+        <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
-
     )
   }
 }
